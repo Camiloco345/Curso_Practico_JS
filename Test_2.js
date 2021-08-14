ca2ab0f520e5c5cee3c72dbcb0f12355bbfcbc13
camiloco345 <camilo.enrique.ovalle@gmail.com>
2021-08-13 20:34:25 -0500

    Creando la conexion entre JS y HTML para proyectar el resultado en la pantalla del navegador y no en la consola con .innerText

--- Test_2.js
+++ Test_2.js
@@ -1,7 +1,22 @@
 
-let CalcularDescuento = (PrecioOriginal, Descuento) =>{
+    let CalcularDescuento = (PrecioOriginal, Descuento) =>{
     const PorcentajePrecioConDescuento = 100 - Descuento
     const PrecioConDescuento = (PrecioOriginal*PorcentajePrecioConDescuento)/100
     
-    console.log("El precio final es: " + PrecioConDescuento)
+    return PrecioConDescuento
+}
+let CalculoDeDescuento = () =>{
+    let Valor = document.getElementById("Valor")
+    let priceValue = Valor.value
+
+    let Descu = document.getElementById("Descu")
+    let DiscountValue = Descu.value
+
+    let PrecioFinal = CalcularDescuento(priceValue,DiscountValue)
+    
+    let FinalPrice = document.getElementById("FinalPrice")
+    
+    FinalPrice.innerText = "El pricio final es:" + PrecioFinal
+
+
 }
