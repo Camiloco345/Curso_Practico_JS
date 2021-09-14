ba22971904571f2a24b62732dfa36f7315dfe827
Camilo Enrique Ovalle Ceron <82371279+Camiloco345@users.noreply.github.com>
2021-09-14 11:34:38 -0500

    Create Salarios.js

---
+++ Salarios.js
@@ -0,0 +1,83 @@
+let Salarios = []
+
+Salarios.push({
+    name: "Camila",
+    salary: 500,
+  });
+  Salarios.push({
+    name: "Nath",
+    salary: 1500,
+  });
+  Salarios.push({
+    name: "Luisa",
+    salary: 1800,
+  });
+  Salarios.push({
+    name: "Laura",
+    salary: 1000,
+  });
+  Salarios.push({
+    name: "Daniela",
+    salary: 2200,
+  });
+  Salarios.push({
+    name: "Esperancita",
+    salary: 200,
+  });
+  Salarios.push({
+    name: "Carla",
+    salary: 500,
+  });
+  Salarios.push({
+    name: "Antonieta",
+    salary: 1500,
+  });
+  Salarios.push({
+    name: "Alicia",
+    salary: 1300,
+  });
+  Salarios.push({
+    name: "Ana",
+    salary: 2400,
+  });
+  Salarios.push({
+    name: "Julia",
+    salary: 3400,
+  });
+  Salarios.push({
+    name: "Rosa",
+    salary: 400,
+  });
+  Salarios.push({
+    name: "Angélica",
+    salary: 400,
+  });
+  Salarios.push({
+    name: "Tatiana",
+    salary: 400,
+  });
+  Salarios.push({
+    name: "Lorena",
+    salary: 600,
+  });
+  Salarios.push({
+    name: "Carolina",
+    salary: 1600,
+  });
+  Salarios.push({
+    name: "Fernanda",
+    salary: 2600,
+  });
+  Salarios.push({
+    name: "Nora",
+    salary: 1000,
+  });
+  Salarios.push({
+    name: "Gisselle",
+    salary: 2000,
+  });
+  Salarios.push({
+    name: "Bill Gates",
+    salary: 100000000,
+  });
+  
